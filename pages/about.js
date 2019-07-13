31660a98026d23b6a8657421a1d640d977ef0703
Talha Ahmad <42ahmad@gmail.com>
2019-07-12 23:46:14 -0400

    making classes for react components

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,14 @@
-const About = () => (
-    <div>
-        <p>This is the about page</p>
-    </div>
-);
+import React from "react";
 
+class About extends React.Component{
+  render(){
+    const data = {text: "this is the about page"};
+
+    return (
+    <div>
+	    <h1>{ data.text }</h1>
+    </div>
+    )
+  }
+}
 export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-const Index = () => (
+import React from "react";
+
+class Index extends React.Component{
+
+render(){
+return (
     <div>
         <Header/>
         <p>Hello Next.js</p>
         <Footer/>
     </div>
 );
+}
 
+}
 export default Index;
