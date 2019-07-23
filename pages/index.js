f22c7ba79a763470519296836cd147ccddcb63d0
Catherine Yuan <catherine.cj.yuan@gmail.com>
2019-07-23 16:14:08 -0500

    Added bio to home page

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,10 @@ import React from "react";
 
 class Index extends React.Component {
 
-    static async getInitialProps( { req }){
-    	const content = {
+    static async getInitialProps({ req }) {
+        const content = {
             slogan: "From the mundane to the insane",
-            wallpapers: ["",""]
+            wallpapers: ["twitter", "instagram", "youtube"]
         };
         return content;
     }
@@ -15,22 +15,37 @@ class Index extends React.Component {
     render() {
         return (
             <Layout>
-                <div style={{
-                    display: "flex",
-                    justifyContent: "center",
-                }}>
-                    <div>{this.props.slogan}</div>
+                <div className="bio">
+                    <div className="wallpaper-container">{this.props.wallpapers.map((wallpaper, i) =>
+                        (
+                            <img height="100" width="100" src={`../static/${wallpaper}.png`} alt="" id={`wallpaper${i}`} className="wallpaper" key={`${i} `} />
+                        ))}
+                    </div>
+                    <div className="slogan-container">{this.props.slogan}</div>
                 </div>
 
                 <style jsx>{`
-                @media screen and (max-width: 600px) {
-                    div {
-                        background-color: yellow;
+                    @media screen and (max-width: 600px) {
+                        .wallpaper {
+                            display: none;
+                        }
+                        #wallpaper1 {
+                            display: block;
+                        }
                     }
-                }
+                    .bio {
+                        display: flex;
+                        flex-direction: column;
+                        align-items: center;
+                        justify-content: center;
+                    }
+                    .wallpaper-container {
+                        display: flex;
+                        flex-direction: row;
+                    }
          
                 `}</style>
-                
+
             </Layout>
         );
     }
