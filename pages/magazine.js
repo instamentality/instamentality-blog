095e8d4a62af2ff757f5b431def1dec29bf0f78e
Talha A <42ahmad@gmail.com>
2019-12-30 23:36:45 -0500

    basic wireframe for header, right nav, main

--- pages/magazine.js
+++ pages/magazine.js
@@ -1,11 +1,43 @@
 import React from "react";
 import Layout from "../components/Layout.js";
 
-class Magazine extends React.Component{
-	render(){
-		return(
+
+const style = {
+	display: "grid",
+	gridTemplateRows: ""
+};
+class Magazine extends React.Component {
+	render() {
+		return (
 			<Layout>
-			  <p>This is the magazine</p>
+				<section>
+					<header>Header</header>
+					<nav>Nav</nav>
+					<main>Main</main>
+				</section>
+
+				<style jsx>{`
+				section {
+					display: grid;
+					grid-template-columns: auto 4rem;
+					grid-template-rows: 2rem auto;
+					grid-template-areas: 
+						"header nav"
+						"main nav"; 
+				}
+				header{
+					grid-area: header;
+				}
+				main{
+					grid-area: main;
+				}
+				nav{
+					grid-area: nav;
+				}
+				
+				`}
+
+				</style>
 			</Layout>
 
 		);
