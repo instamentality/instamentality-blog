409b7cb974ec5b52f05b9b0199ddc45b9488806b
Talha A <42ahmad@gmail.com>
2019-12-31 20:23:55 -0500

    better hooks refactor

--- components/Menu.js
+++ components/Menu.js
@@ -4,14 +4,10 @@ const Menu = () => {
     //using react hooks
     const baseClasses = "hamburger hamburger--collapse"
     const [active, setActive] = useState(false)
-    const [classes, setClasses] = useState("hamburger hamburger--collapse")
-    useEffect(() => { 
-        active ? setClasses(`${baseClasses} is-active`) : setClasses(baseClasses)
-    }, [active])
     return (
         <button
             onClick={() => setActive(!active)}
-            className={classes} type="button">
+            className={active ? `${baseClasses} is-active` : baseClasses} type="button">
             <span className="hamburger-box">
                 <span className="hamburger-inner"></span>
             </span>
